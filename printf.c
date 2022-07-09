30f0bdf8dbb55ae6c47f8ea1ed2f2a47f1bcb7db
Munyorodee <munyoro.md95@gmail.com>
2022-07-09 10:06:29 -0700

    printf.c

--- printf.c
+++ printf.c
@@ -1,50 +1,93 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _printf - prints formatted data to stdout
- * @format: string that contains the format to print
- * Return: number of characters written
+ * map_specifiers - maps specifiers with their corresponding
+ * functions.
+ *
+ * @c: The next character after %.
+ * @ap: The argument parameter.
+ *
+ * Return: The corresponding function to the specifier
+ *
  */
-int _printf(char *format, ...)
+
+int map_specifiers(char c, va_list ap)
 {
-	int written = 0, (*structype)(char *, va_list);
-	char q[3];
-	va_list pa;
+	int index = 0;
+
+	converter func_cor[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_int},
+		{"i", print_int},
+		{"b", print_b},
+		{"o", print_o},
+		{"u", print_u},
+		{"x", print_x},
+		{"X", print_X},
+		{"S", print_S},
+
+		{NULL, NULL}
+	};
+
+	for (; func_cor[index].specifier != NULL; index++)
+	{
+		if (func_cor[index].specifier[0] == c)
+			return (func_cor[index].func(ap));
+	}
+
+	return (0);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print. It may conatain directives
+ *
+ * Return: Returns the number of characters to be printed. (excluding the
+ * null byte used to end output to strings). if it fails it returns -1.
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+	int ch_count = 0, spec = 0, i = 0;
+
+	va_list ap;
+
+	va_start(ap, format);
 
 	if (format == NULL)
 		return (-1);
-	q[2] = '\0';
-	va_start(pa, format);
-	_putchar(-1);
-	while (format[0])
+
+	for (; *(format + i) != '\0'; i++)
 	{
-		if (format[0] == '%')
+		if (*(format + i) != '%')
 		{
-			structype = driver(format);
-			if (structype)
-			{
-				q[0] = '%';
-				q[1] = format[1];
-				written += structype(q, pa);
-			}
-			else if (format[1] != '\0')
-			{
-				written += _putchar('%');
-				written += _putchar(format[1]);
-			}
-			else
-			{
-				written += _putchar('%');
-				break;
-			}
-			format += 2;
+			_putchar(*(format + i));
+			ch_count++;
+			continue;
 		}
-		else
+		if (*(format + i + 1) == '%')
 		{
-			written += _putchar(format[0]);
-			format++;
+			_putchar('%');
+			ch_count++;
+			i++;
+			continue;
+		}
+		if (*(format + i + 1) == '\0')
+			return (-1);
+		spec = map_specifiers(*(format + i + 1), ap);
+
+		if (spec == -1 || spec != 0)
+			i++;
+		if (spec > 0)
+			ch_count += spec;
+		if (spec == 0)
+		{
+			_putchar('%');
+			ch_count++;
 		}
 	}
-	_putchar(-2);
-	return (written);
+	va_end(ap);
+	return (ch_count);
 }
